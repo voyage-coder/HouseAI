1b4f40c67c752049f5ec2e92de064297c07f9a66
voyage-coder <koppineedisree9@gmail.com>
2026-07-04 03:31:25 +0530

    Changed api.js url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
